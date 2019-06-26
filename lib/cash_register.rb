d0a915a143f34d44270254e6f9c430572701059e
Tracy Reuther <tracyreuther@gmail.com>
2019-06-26 17:03:45 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,25 @@
+class CashRegister
+  
+attr_accessor :total, :discount, :price, :total_items
 
+def initialize(discount = 0)
+  @total = 0
+  @discount = discount
+  @total_items = []
+end
+
+def add_item(item, price, quantity = 1)
+  @price = price
+  @total += price * quantity
+  counter = 0
+  while counter < quantity
+    @total_items << item
+    counter += 1
+  end
+end
+
+def apply_discount
+  
+end
+
+end
